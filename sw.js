6a65efc9ee0eac082dcdad6e22881bc850cc63fc
joaoibira-ux <joao.ibira@gmail.com>
2026-06-06 23:17:06 -0300

    gw-redesign v1: redesign glassmorphism do Sistema GW
    Copia experimental do Sistema GW com novo padrao visual: cards de vidro
    fosco (blur/transparencia) sobre imagens de fundo em todas as telas
    (PIN, Home, aviso de instalacao e instalar.html).

---
+++ sw.js
@@ -0,0 +1,13 @@
+const VERSION = "gw-redesign-v1";
+const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-home.jpg", "./bg-aviso.jpg", "./bg-instalar.jpg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.mode === "navigate") { e.respondWith(fetch(e.request).catch(() => caches.match("./index.html"))); return; }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
