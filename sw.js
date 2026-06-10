532db27471adc13d92549bb6236bf562fbdaf2b4
joaoibira-ux <joao.ibira@gmail.com>
2026-06-10 09:39:40 -0300

    ponto v2.4 / v2.9: corrige reaparecimento da tela de confirmacao apos "rosto nao reconhecido"
    Adiciona flag recognizing para impedir chamadas concorrentes a
    reconhecerFace(). Em dispositivos mais lentos, o calculo do
    descritor facial podia ultrapassar o intervalo de 2200ms, disparando
    uma segunda deteccao em paralelo: a primeira reconhecia o
    funcionario e abria a confirmacao, e a segunda (ja em andamento)
    concluia depois como "desconhecido" e mostrava ROSTO NAO
    RECONHECIDO; ao tocar em "Tentar novamente" o painel de confirmacao
    (que continuava com a classe active) reaparecia.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v33";
+const VERSION = "gw-redesign-v34";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
