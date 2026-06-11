22da3f17ab08cdb1146f587e17febd1965249865
joaoibira-ux <joao.ibira@gmail.com>
2026-06-10 23:15:09 -0300

    v3.7: bump por uso do "Nome no Mapa" também no mapa do app Folha (v4.60)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v41";
+const VERSION = "gw-redesign-v42";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
