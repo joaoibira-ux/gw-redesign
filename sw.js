b0b56fb0202268460ab45f553e4ad4ee2688a11f
joaoibira-ux <joao.ibira@gmail.com>
2026-06-11 02:02:56 -0300

    v3.14/gw-redesign-v49: ponto v2.9 - bloqueia uso fora do modo app (instalado pelo icone) tambem em desktop, nao so mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v48";
+const VERSION = "gw-redesign-v49";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
