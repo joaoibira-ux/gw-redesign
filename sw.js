cd6ca01b5c7c6284d4e9eb8366754f7d1c2cac19
joaoibira-ux <joao.ibira@gmail.com>
2026-06-11 12:24:35 -0300

    v3.18/gw-redesign-v53: bump por nova origem ANE->CREDITO A REPASSAR P BBS FOMENTO no Caixa v3.26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v52";
+const VERSION = "gw-redesign-v53";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
