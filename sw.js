6c3b609e2585b298d6b5435117bcf1965508bba0
joaoibira-ux <joao.ibira@gmail.com>
2026-06-12 15:39:51 -0300

    v3.42/caixa-v1.67: Previsao da Folha de Pagamento usa Bruto recalculado em tempo real a partir de locais/servicos (corrige valores desatualizados quando o status de servicos muda apos o fechamento da folha)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v76";
+const VERSION = "gw-redesign-v77";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
