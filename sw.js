0de748d9adbe309a17f3a4fe90b90fe04688602c
joaoibira-ux <joao.ibira@gmail.com>
2026-07-17 08:58:34 -0300

    fix(pin): encolher teclado numérico em telas largas (PC) pra caber sem rolagem
    Layout era dimensionado pra celular; numa janela de PC o teclado gigante
    empurrava o checkbox "confiar neste computador" pra fora da área
    visível mesmo com a rolagem já habilitada. Media query (min-width:700px)
    encolhe logo, cartão do PIN e teclado proporcionalmente.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-redesign-v4.35";
+const VERSION = "gw-redesign-v4.36";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png", "./bg-pin.jpg", "./bg-instalar.jpg"];
 
 self.addEventListener("install", e => {
